2364f0cc79cded283544c72f8ef0bb97f96b7bfc
surveybaja-source <survey.baja@gmail.com>
2026-03-15 13:23:48 -0700

    Add Home component for Survey Link page

---
+++ app/page.js
@@ -0,0 +1,14 @@
+'use client'
+
+export default function Home() {
+  return (
+    <div style={{minHeight:'100vh',background:'#0c1a27',display:'flex',alignItems:'center',justifyContent:'center'}}>
+      <div style={{textAlign:'center'}}>
+        <h1 style={{color:'#dd2e1e',fontFamily:'sans-serif',fontSize:48,fontWeight:900}}>
+          Survey<span style={{color:'#ffffff'}}>Link</span>
+        </h1>
+        <p style={{color:'#8fa8c0',marginTop:16}}>Marine Cargo Survey Platform</p>
+      </div>
+    </div>
+  )
+}
